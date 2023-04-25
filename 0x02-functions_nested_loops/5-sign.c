562a561bde56a835bcf35246a89a66065377f751
Teresa-ops <trsmwaura@gmail.com>
2023-04-25 04:24:57 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - Prints the sign of a number
- * @n: the int to check
+ * @n: The int to check
  * Return: 1 and prints + n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if  n is less than zero
